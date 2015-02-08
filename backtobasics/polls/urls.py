588826c3fb4c728de9f742b1b4ecd272e2cc3e63
Ian Kosen <ikosenn@gmail.com>
2015-02-08 18:39:01 +0300

    Finished part 4

--- backtobasics/polls/urls.py
+++ backtobasics/polls/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, patterns
 
-from .views import index, detail,vote,results
+from .views import *
 
 urlpatterns = [
 	#ex: /polls/
-	url(r'^$', index,name = 'index'),
+	url(r'^$', IndexView.as_view(),name = 'index'),
 
 	#ex: /polls/5/
-	url(r'^(?P<question_id>[0-9]+)/$', detail, name = 'detail'),
-	url(r'^(?P<question_id>[0-9]+)/results/$',results, name = 'results'),
+	url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name = 'detail'),
+	url(r'^(?P<pk>[0-9]+)/results/$',ResultsView.as_view(), name = 'results'),
 	url(r'^(?P<question_id>[0-9]+)/vote/$',vote, name = 'vote'),
 ]
